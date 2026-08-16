8ad6787747331c1739440bc987be68606676f8bc
Ayda84 <aydaahadadii847@gmail.com>
2026-08-16 14:59:28 +0330

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Menu from "./Menu";
 import Footer from "./Footer";
+import loadingGif from "../image/loading/Infinity@1x-1.2s-199px-199px.gif";
 
 export default function Loading() {
   const { pathname } = useLocation();
@@ -23,7 +24,7 @@ export default function Loading() {
       <Menu />
       <div className="fixed inset-0 bg-white/80 flex items-center justify-center z-[9999]">
         <img
-          src="/src/image/loading/Infinity@1x-1.2s-199px-199px.gif"
+          src={loadingGif}
           alt="loading"
           className="md:w-32 md:h-32 w-24 h-24"
         />
