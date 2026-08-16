37a8f686a5951e427a4a8df0554db494ca38d6e3
Ayda84 <aydaahadadii847@gmail.com>
2026-08-16 14:37:57 +0330

    Update Myslide.jsx

--- src/components/Myslide.jsx
+++ src/components/Myslide.jsx
@@ -1,19 +1,25 @@
-// وارد کردن کامپوننت‌های اصلی
+
 import { Swiper, SwiperSlide } from "swiper/react";
 
-// وارد کردن ماژول‌ها از مسیر صحیح
+
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
-// وارد کردن استایل‌ها
+
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+
+
+import pic1 from "../image/slider/pic1.webp";
+import pic2 from "../image/slider/pic2.webp";
+import pic3 from "../image/slider/pic3.webp";
+import pic4 from "../image/slider/pic4.webp";
+import pic5 from "../image/slider/pic5.webp";
 
 const MySlider = () => {
   return (
     <div className="w-full md:mt-3 mt-6">
       <Swiper
-        // اینجا حتما باید Autoplay را در آرایه ماژول‌ها قرار دهی
         modules={[Autoplay, Pagination, Navigation]}
         spaceBetween={10}
         slidesPerView={1}
@@ -26,22 +32,22 @@ const MySlider = () => {
         className="w-full"
       >
         <SwiperSlide className="w-full h-128 flex items-center justify-center text-white text-2xl w-full">
-          <img src="./src/image/slider/pic1.webp" alt="slider" />
+          <img src={pic1} alt="slider" />
         </SwiperSlide>
         <SwiperSlide className="w-full h-128 flex items-center justify-center text-white text-2xl">
-          <img src="./src/image/slider/pic2.webp" alt="slider" />
-        </SwiperSlide>
-
-        <SwiperSlide className="w-full h-128 flex items-center justify-center text-white text-2xl">
-          <img src="./src/image/slider/pic3.webp" alt="slider" />
+          <img src={pic2} alt="slider" />
         </SwiperSlide>
 
         <SwiperSlide className="w-full h-128 flex items-center justify-center text-white text-2xl">
-          <img src="./src/image/slider/pic4.webp" alt="slider" />
+          <img src={pic3} alt="slider" />
         </SwiperSlide>
 
         <SwiperSlide className="w-full h-128 flex items-center justify-center text-white text-2xl">
-          <img src="./src/image/slider/pic5.webp" alt="slider" />
+          <img src={pic4} alt="slider" />
+        </SwiperSlide>
+
+        <SwiperSlide className="w-full h-128 flex items-center justify-center text-white text-2xl">
+          <img src={pic5} alt="slider" />
         </SwiperSlide>
       </Swiper>
     </div>
