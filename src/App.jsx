211af393c3fa82d0f2f4f0471487cfa067433a79
Ayda84 <aydaahadadii847@gmail.com>
2026-08-17 14:21:32 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ export default function App() {
       <Producrprovider>
         <AuthProvider>
           <CartProvider>
-            <Router>
+            <Router basename="/minishop/">
               <ScrollToTop />
               <Loading />
               {welcomeName && (
